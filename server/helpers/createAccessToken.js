a8b9fa62d8a3540345ffd464b34fd703a2d9d927
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2021-07-09 21:13:05 +0100

    feat: read from JSON directly

--- server/helpers/createAccessToken.js
+++ server/helpers/createAccessToken.js
@@ -1,12 +1,9 @@
 const fetch = require('node-fetch')
-const path = require('path')
-const { readFileSync } = require('fs')
+const truIdConfig = require('../../tru.json')
+
 exports.createAccessToken = async () => {
   // make request body acceptable by application/x-www-form-urlencoded
-  const truIdConfigPath = path.join(__dirname, '../../tru.json')
-  console.log(truIdConfigPath)
-  const data = readFileSync(truIdConfigPath, 'utf-8')
-  const truIdConfig = JSON.parse(data)
+
   const clientId = truIdConfig.credentials[0].client_id
   const clientSecret = truIdConfig.credentials[0].client_secret
 
