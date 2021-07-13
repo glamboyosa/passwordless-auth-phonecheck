7b13cae647314929ccf3aefd99664f0e8ffd0b49
glamboyosa <ogbemudiatimothy@gmail.com>
2021-07-13 16:41:00 +0100

    added tru logo

--- mobile/src/Screens.js
+++ mobile/src/Screens.js
@@ -94,9 +94,7 @@ const Screens = () => {
           <View style={styles.box}>
             <Image
               style={styles.logo}
-              source={{
-                uri: 'https://gitlab.com/4auth/devx/tru-id-developer/uploads/0b58ca5c079efba056699a5d94eea73d/color_normal_vertical_4x.png',
-              }}
+              source={require('./images/tru-logo.png')}
             />
             <Text style={styles.heading}>Register</Text>
             <TextInput
