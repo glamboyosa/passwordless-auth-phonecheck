87efec07b064013506c46bfc32be0417c51ceab8
Greg Holmes <greg.holmes@tru.id>
2021-08-17 16:32:09 +0100

    Added final changes and additions to server for the complete tutorial

--- server/index.js
+++ server/index.js
@@ -1,16 +1,53 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const { createPhoneCheck } = require('./helpers/createPhoneCheck')
+const { getPhoneCheck } = require('./helpers/getPhoneCheck')
 
 app.use(express.json())
 app.use(morgan('dev'))
 
 // create PhoneCheck
-app.post('/api/register', async (req, res) => {})
+app.post('/api/register', async (req, res) => {
+  const { phone_number: phoneNumber } = req.body
+
+  try {
+    // create PhoneCheck resource
+
+    const { checkId, checkUrl, numberSupported } = await createPhoneCheck(
+      phoneNumber,
+    )
+
+    if (!numberSupported) {
+      res.status(400).send({ message: 'number not supported' })
+    } else {
+      res.status(201).send({
+        data: { checkId, checkUrl },
+        message: 'PhoneCheck created',
+      })
+    }
+  } catch (e) {
+    res.status(500).send({ message: e.message })
+  }
+})
 
 // get PhoneCheck response
 
-app.get('/api/register', async (req, res) => {})
+app.get('/api/register', async (req, res) => {
+  // get the `check_id` from the query parameter
+  const { check_id: checkId } = req.query
+
+  try {
+    // get the PhoneCheck response
+    const { match } = await getPhoneCheck(checkId)
+
+    console.log(match)
+    res.status(200).send({ data: { match } })
+  } catch (e) {
+    console.log(JSON.stringify(e))
+    res.status(500).send({ message: e.message })
+  }
+})
 
 // setup server
 app.listen(4000, () => {
