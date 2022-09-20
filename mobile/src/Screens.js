f465a820c328c6895d14a9dcf3c6c2151b0a414a
Greg Holmes <greg.holmes@tru.id>
2022-09-20 12:01:13 +0100

    Updated src/Screens.js to contain new functionality for v0.2 and new SDK

--- mobile/src/Screens.js
+++ mobile/src/Screens.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from 'react'
+import TruSdkReactNative from '@tru_id/tru-sdk-react-native'
 
 import {
   SafeAreaView,
@@ -18,11 +19,163 @@ import LinearGradient from 'react-native-linear-gradient'
 import { AuthContext } from './Context'
 
 const Screens = () => {
-  // server ngrok url
-  const base_url = 'https://serverngrokurl.ngrok.io'
-  const { screen } = useContext(AuthContext)
+  const base_url = '{YOUR_NGROK_URL}'
+  const { screen, setScreen } = useContext(AuthContext)
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  const registerHandler = () => {}
+  const errorHandler = ({ title, message }) => {
+    return Alert.alert(title, message, [
+      {
+        text: 'Close',
+        onPress: () => console.log('Alert closed'),
+      },
+    ])
+  }
+
+  const registerHandler = async () => {
+    const body = { phone_number: phoneNumber }
+
+    setLoading(true)
+
+    console.log('creating PhoneCheck for', body)
+
+    try {
+      const reachabilityResponse =
+      await TruSdkReactNative.openWithDataCellular(
+        'https://{DATA_RESIDENCY}.api.tru.id/public/coverage/v0.1/device_ip'
+      );
+
+      console.log(reachabilityResponse);
+      let isMNOSupported = false
+
+      if ('error' in reachabilityResponse) {
+        errorHandler({
+          title: 'Something went wrong.',
+          message: 'MNO not supported',
+        })
+        setLoading(false)
+
+        return
+      } else if ('http_status' in reachabilityResponse) {
+        let httpStatus = reachabilityResponse.http_status;
+        if (httpStatus === 200 && reachabilityResponse.response_body !== undefined) {
+          let body = reachabilityResponse.response_body;
+          console.log('product => ' + JSON.stringify(body.products[0]));
+          isMNOSupported = true;
+        } else if (httpStatus === 400 || httpStatus === 412 || reachabilityResponse.response_body !== undefined) {
+          errorHandler({
+            title: 'Something went wrong.',
+            message: 'MNO not supported',
+          })
+          setLoading(false)
+
+          return
+        }
+      }
+
+      let isPhoneCheckSupported = false
+
+      if (isMNOSupported === true) {
+        reachabilityResponse.response_body.products.forEach((product) => {
+          console.log('supported products are', product)
+
+          if (product.product_name === 'Phone Check') {
+            isPhoneCheckSupported = true
+            console.log('Phone Check is here!')
+          }
+        })
+      }
+
+      if (!isPhoneCheckSupported) {
+        setLoading(false)
+        errorHandler({
+          title: 'Something went wrong.',
+          message: 'PhoneCheck is not supported on MNO',
+        })
+        return
+      }
+
+      const response = await fetch(`${base_url}/api/register`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      const data = await response.json()
+
+      console.log(data)
+
+      console.log(`PhoneCheck [Start] ->`);
+      const checkResponse = await TruSdkReactNative.openWithDataCellular(
+        data.data.checkUrl
+      );
+      console.log(`PhoneCheck [Done] ->`);
+
+      if ('error' in checkResponse) {
+        console.log(`Error in openWithDataCellular: ${checkResponse.error_description}`);
+      } else if ('http_status' in checkResponse) {
+        const httpStatus = checkResponse.http_status;
+        if (httpStatus === 200 && checkResponse.response_body !== undefined) {
+          console.log(`Requesting PhoneCheck URL`);
+
+          if ('error' in checkResponse.response_body) {
+            const body = checkResponse.response_body;
+            console.log(`Error: ${body.error_description}`);
+          } else {
+            const body = checkResponse.response_body;
+
+            try {
+              const checkStatusRes = await fetch(`${base_url}/api/exchange-code`, {
+                method: 'POST',
+                body: JSON.stringify({
+                  check_id: body.check_id,
+                  code: body.code,
+                  reference_id: null,
+                }),
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              })
+
+              const data = await checkStatusRes.json()
+              console.log('[CHECK RESULT]:', data);
+
+              if (data.data.match) {
+                console.log(`✅ successful PhoneCheck match`);
+                setLoading(false)
+                setPhoneNumber('')
+                setScreen('home')
+              } else {
+                console.log(`❌ failed PhoneCheck match`);
+                setLoading(false)
+                errorHandler({
+                  title: 'Registration Failed',
+                  message: 'PhoneCheck match failed. Please contact support',
+                })
+              }
+            } catch (error) {
+              console.log(`Error: ${error.message}`);
+              console.log(JSON.stringify(error, null, 2));
+              errorHandler({
+                title: 'Error retrieving check result',
+                message: error.message,
+              })
+              return;
+            }
+          }
+        } else {
+          const body = resp.response_body;
+          console.log(`Error: ${body.detail}`);
+        }
+      }
+    } catch (e) {
+      setLoading(false)
+      errorHandler({ title: 'Something went wrong', message: e.message })
+    }
+  }
 
   return (
     <LinearGradient
@@ -41,6 +194,28 @@ const Screens = () => {
               source={require('./images/tru-logo.png')}
             />
             <Text style={styles.heading}>Register</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Number ex. +448023432345"
+              placeholderTextColor="#d3d3d3"
+              keyboardType="phone-pad"
+              value={phoneNumber}
+              editable={!loading}
+              onChangeText={(value) =>
+                setPhoneNumber(value.replace(/\s+/g, ''))
+              }
+            />
+            {loading ? (
+              <ActivityIndicator
+                style={styles.spinner}
+                size="large"
+                color="#00ff00"
+              />
+            ) : (
+              <TouchableOpacity onPress={registerHandler} style={styles.button}>
+                <Text style={styles.buttonText}>Register</Text>
+              </TouchableOpacity>
+            )}
           </View>
         </SafeAreaView>
       ) : (
@@ -53,6 +228,7 @@ const Screens = () => {
     </LinearGradient>
   )
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
