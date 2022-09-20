12080ec3d5645af8d7e4d9749a1ed9b6f92cee6d
Greg Holmes <greg.holmes@tru.id>
2022-09-20 12:02:57 +0100

    Updated server to contain new v0.2 functionality

--- server/index.js
+++ server/index.js
@@ -1,16 +1,51 @@
 const express = require('express')
 const morgan = require('morgan')
+const { createPhoneCheck } = require('./helpers/createPhoneCheck')
+const { patchPhoneCheck } = require('./helpers/patchPhoneCheck')
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
 
 // create PhoneCheck
-app.post('/api/register', async (req, res) => {})
+app.post('/api/register', async (req, res) => {
+  const { phone_number: phoneNumber } = req.body
 
-// get PhoneCheck response
+  try {
+    // create PhoneCheck resource
+    const { checkId, checkUrl, numberSupported } = await createPhoneCheck(
+      phoneNumber,
+    )
 
-app.get('/api/register', async (req, res) => {})
+    if (!numberSupported) {
+      res.status(400).send({ message: 'number not supported' })
+    } else {
+      res.status(201).send({
+        data: { checkId, checkUrl },
+        message: 'PhoneCheck created',
+      })
+    }
+  } catch (e) {
+    res.status(500).send({ message: e.message })
+  }
+})
+
+// complete PhoneCheck
+app.post('/api/exchange-code', async (req, res) => {
+  // get the `check_id` from the query parameter
+  const { check_id: checkId, code: code } = req.body
+
+  try {
+    // get the PhoneCheck response
+    const { patchResponseStatus, patchResponse } = await patchPhoneCheck(checkId, code)
+
+    console.log(patchResponseStatus, patchResponse)
+    res.status(patchResponseStatus).send({ data: patchResponse })
+  } catch (e) {
+    console.log(JSON.stringify(e))
+    res.status(500).send({ message: e.message })
+  }
+})
 
 // setup server
 app.listen(4000, () => {
